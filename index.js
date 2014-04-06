c6a8f10d8dfc295c6a6812665121ee570ac97141
smonusbonus <simokrei@gmail.com>
2014-04-06 17:30:42 +0200

    added public folder and changed index.js to serve index.html file

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  //res.send('Hello World!');
+  res.sendfile('public/index.html');
 });
 
 var port = Number(process.env.PORT || 5000);
