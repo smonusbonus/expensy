1e98dcbc7a74e96c669c9ccf85b1a8aa149d7c01
smonusbonus <simokrei@gmail.com>
2014-04-06 17:15:25 +0200

    fixed dependencies error

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-// hello world example
-/*var http = require('http');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(process.env.PORT || 1337)
-
-console.log('Server running at http://127.0.0.1:1337/');*/
-
 // simple web app using the express framework for node
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-  res.send('Hello World');
+app.get('/', function(req, res) {
+  res.send('Hello World!');
 });
 
-var server = app.listen((process.env.PORT || 3000), function() {
-    console.log('Listening on port %d', server.address().port);
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
