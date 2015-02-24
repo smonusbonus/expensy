cac086c0940dc4f9e3735b3c33a066531c44dccf
smonusbonus <simokrei@gmail.com>
2015-02-24 18:55:41 +0100

    large restructure of files and improved grunt build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,64 +4,111 @@ module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     concat: {
-      distControllers: {
+      distApp: {
         // the files to concatenate
-        src: ['app/js/controllers/*.js'],
+        src: ['src/app/*.js'],
         // the location of the resulting JS file
-        dest: 'app/js/controllers/concat-min/controllers.concat.js'
+        dest: 'build/js/app.js'
       },
-      distModels: {
+      distComponents: {
         // the files to concatenate
-        src: ['app/js/models/*.js'],
+        src: ['src/app/components/**/*.js'],
         // the location of the resulting JS file
-        dest: 'app/js/models/concat-min/models.concat.js'
+        dest: 'build/js/components.js'
       },
-      distDirectives: {
+      distShared: {
         // the files to concatenate
-        src: ['app/js/directives/*.js'],
+        src: ['src/app/shared/*.js'],
         // the location of the resulting JS file
-        dest: 'app/js/directives/concat-min/directives.concat.js'
-      }
-    },
+        dest: 'build/js/shared.js'
+      },
+    },/*
     uglify: {
       options: {
         mangle: false
       },
       buildControllers: {
-        src: 'app/js/controllers/*.js',
-        dest: 'app/js/controllers/concat-min/controllers.concat.min.js'
+        src: 'tmp/js/controllers.concat.js',
+        dest: 'build/js/controllers.concat.min.js'
       },
       buildModels: {
-        src: 'app/js/models/*.js',
-        dest: 'app/js/models/concat-min/models.concat.min.js'
+        src: 'tmp/js/models.concat.js',
+        dest: 'build/js/models.concat.min.js'
       },
       buildDirectives: {
-        src: 'app/js/directives/*.js',
-        dest: 'app/js/directives/concat-min/directives.concat.min.js'
+        src: 'tmp/js/directives.concat.js',
+        dest: 'build/js/directives.concat.min.js'
       },
       buildMainJs: {
-        src: 'app/js/app.js',
-        dest: 'app/js/app.min.js'
+        src: 'src/js/app.js',
+        dest: 'build/js/app.min.js'
       },
-    },
+    },*/
     sass: { // Task
       dist: { // Target
         options: { // Target options
           style: 'compressed'
         },
         files: { // Dictionary of files
-          'app/css/app.min.css': 'app/css/scss/app.scss' // 'destination': 'source'
-        }
-      }
-    }
+          'build/css/app.css': 'src/assets/scss/app.scss' // 'destination': 'source'
+        },
+      },
+    },
+    copy: {
+      main: {
+        files: [
+          {expand: true,  cwd: 'src/', src: ['app/**/*.html'], dest: 'build/', filter: 'isFile'},
+
+          // copy font files manually (bower-concat doesn't do it)
+          {expand: true,  cwd: 'bower_components/fontawesome/', src: ['fonts/*'], dest: 'build/', filter: 'isFile'},
+          {expand: true,  cwd: 'bower_components/ratchet/', src: ['fonts/*'], dest: 'build/', filter: 'isFile'},
+     
+          {src: ['src/index.html'], dest: 'build/index.html'},
+        ],
+      },
+    },
+    bower_concat: {
+      all: {
+        dest: 'build/js/bower.js',
+        cssDest: 'build/css/bower.css',
+        exclude: [
+
+        ],
+        dependencies: {
+
+        },
+        bowerOptions: {
+          relative: false
+        },
+      },
+    },
+    watch: {
+      scripts: {
+        files: ['src/app/**/*.js'],
+        tasks: ['concat', 'copy'],
+        options: {
+          spawn: false,
+        },
+      },
+      html: {
+        files: ['src/**/*.html'],
+        tasks: ['copy'],
+        options: {
+          spawn: false,
+        },
+      },
+    },
   });
 
   // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-bower-concat');  
   grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-copy');  
+  grunt.loadNpmTasks('grunt-contrib-uglify');  
+  grunt.loadNpmTasks('grunt-contrib-sass');  
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
-  //grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['concat', /*'uglify', */'sass', 'copy', 'bower_concat']);
 
 };
---
+++ src/app/app.routes.js
@@ -0,0 +1,32 @@
+'use strict';
+
+expenseTrackerAppModule.config(['$routeProvider', function ($routeProvider) {
+
+  // main navigation
+  $routeProvider.when('/expenses/add', {templateUrl: 'app/components/expense/expense-add.html', controller: 'expenseTracker.expensesController'});
+  $routeProvider.when('/expenses/add/category', {templateUrl: 'app/components/expense/expense-add-category.html', controller: 'expenseTracker.expensesController'});
+  $routeProvider.when('/expenses/add/details', {templateUrl: 'app/components/expense/expense-add-details.html', controller: 'expenseTracker.expensesController'});
+  $routeProvider.when('/expenses/remove/:id', {templateUrl: 'app/components/expense/expense-add.html', controller: 'expenseTracker.expensesController'});
+
+  $routeProvider.when('/feed', {templateUrl: 'app/components/feed/feed.html', controller: 'expenseTracker.feedController'});
+  $routeProvider.when('/feed/detail/:id', {templateUrl: 'app/components/feed/feed-detail.html', controller: 'expenseTracker.feedController'});
+
+  $routeProvider.when('/overview', {templateUrl: 'app/components/overview/overview.html', controller: 'expenseTracker.overviewController'});
+  $routeProvider.when('/settings', {templateUrl: 'app/components/settings/settings.html', controller: 'expenseTracker.settingsController'});
+  
+  $routeProvider.when('/overview/weekly', {templateUrl: 'app/components/overview/overview-weekly.html', controller: 'expenseTracker.overviewController'});
+  
+  $routeProvider.when('/goals', {templateUrl: 'app/components/goals/goals.html', controller: 'expenseTracker.goalsController'});
+  
+  $routeProvider.when('/settings/goals/add', {templateUrl: 'app/components/goals/goals-add.html', controller: 'expenseTracker.goalsController'});
+
+  $routeProvider.when('/settings/budget', {templateUrl: 'app/components/settings/budget.html', controller: 'expenseTracker.settingsController'});
+  $routeProvider.when('/settings/change-currency', {templateUrl: 'app/components/settings/change-currency.html', controller: 'expenseTracker.settingsController'});
+  $routeProvider.when('/settings/maximum-per-spending', {templateUrl: 'app/components/settings/maximum-per-spending.html', controller: 'expenseTracker.settingsController'});
+  
+  $routeProvider.when('/settings/categories', {templateUrl: 'app/components/categories/categories.html', controller: 'expenseTracker.categoriesController'});
+  $routeProvider.when('/settings/categories/add', {templateUrl: 'app/components/categories/categories-add.html', controller: 'expenseTracker.categoriesController'});
+  
+  // root path
+  $routeProvider.otherwise({redirectTo: '/expenses/add'});
+}]);
